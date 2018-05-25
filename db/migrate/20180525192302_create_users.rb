b671cc6c694edb03bd2191aa2470d9f4627b47e0
Patrick Robertson <patricksrobertson@gmail.com>
2018-05-25 15:23:52 -0400

    Create users table.

---
+++ db/migrate/20180525192302_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :discord_name
+      t.string :wow_name
+      t.string :wow_server
+      t.string :status, default: 'needs_birb'
+      t.date :status_date
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_05_25_192302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "discord_name"
+    t.string "wow_name"
+    t.string "wow_server"
+    t.string "status", default: "needs_birb"
+    t.date "status_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
