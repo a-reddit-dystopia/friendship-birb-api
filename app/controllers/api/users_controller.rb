bdcce83725cf078e6211f762dacdf81f71662cad
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-19 11:00:11 -0400

    Add discord_id.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,14 +6,25 @@ module Api
     end
 
     def create
-      user = User.create(user_params)
+      user = User.create(create_params)
       render json: UserSerializer.new(user).serialized_json, status: 201
+    end
+
+    def update
+      user = User.find(params[:id])
+      user.update(update_params)
+
+      render json: UserSerializer.new(user).serialized_json, status: 200
     end
 
     protected
 
-    def user_params
-      params.require(:user).permit(:discord_name, :wow_name, :wow_server, :status, :status_date)
+    def create_params
+      params.require(:user).permit(:discord_name, :wow_name, :wow_server, :status, :status_date, :discord_id)
+    end
+
+    def update_params
+      params.require(:user).permit(:status, :status_date)
     end
   end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :discord_name, :wow_name, :wow_server, :status, :status_date
+  attributes :discord_name, :discord_id, :wow_name, :wow_server, :status, :status_date
 end
