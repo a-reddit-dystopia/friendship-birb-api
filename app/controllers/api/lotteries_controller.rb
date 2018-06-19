49133a76d96adef4663a857a7c8ea5800f91796d
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-19 12:25:30 -0400

    Crash a little less on the lottery command.

--- app/controllers/api/lotteries_controller.rb
+++ app/controllers/api/lotteries_controller.rb
@@ -2,10 +2,14 @@ module Api
   class LotteriesController < ApiController
     def create
       user = User.where(status: 'active').limit(1).order("RANDOM()").first
-      lottery = Lottery.create(user_id: user.id)
-      user.update(status: 'inactive', status_date: DateTime.current)
+      if user?
+        lottery = Lottery.create(user_id: user.id)
+        user.update(status: 'inactive', status_date: DateTime.current)
 
-      render json: UserSerializer.new(user).serialized_json, status: 201
+        render json: UserSerializer.new(user).serialized_json, status: 201
+      else
+        render json: {error: 'no more drawings!'}, status: 422
+      end
     end
   end
 end
