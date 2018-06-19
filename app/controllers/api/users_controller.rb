469c3a699a75b68d18c5c203d8356869d610681d
Patrick Robertson <patricksrobertson@gmail.com>
2018-06-19 09:48:04 -0400

    I should have been less outraged.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ module Api
   class UsersController < ApiController
     def index
       users = User.all
-      render json: ::UserSerialzer.new(users).serialized_json, status: 200
+      render json: UserSerializer.new(users).serialized_json, status: 200
     end
 
     def create
