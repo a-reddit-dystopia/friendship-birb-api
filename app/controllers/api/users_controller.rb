5daa09e8c89047b7cf86d1357b00c99a56e9bae2
Patrick Robertson <patricksrobertson@gmail.com>
2018-07-01 06:35:32 -0400

    return user count instead of users.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,8 @@
 module Api
   class UsersController < ApiController
     def index
-      users = User.where(status: 'active').order('created_at desc').limit(25)
-      render json: UserSerializer.new(users).serialized_json, status: 200
+      users = User.where(status: 'active').count
+      render json: { count: users }, status: 200
     end
 
     def create
